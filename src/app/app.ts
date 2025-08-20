73802ac9982c2ea519cef765ed49b7877138c3c0
ratho3009 <ratho3009@oslomet.no>
2025-08-20 11:22:30 +1000

    logout implement

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive], // <-- legg til
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {}
+
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('currentUser');
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.router.navigate(['/login']);
+  }
+}
